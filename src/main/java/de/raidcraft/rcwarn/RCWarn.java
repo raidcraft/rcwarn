cb627578cd3b89f57c75b54d82146a4857208b60
Philip Urban <urban.philip@googlemail.com>
2013-02-03 14:51:55 +0100

    added automatic thread creation

--- src/main/java/de/raidcraft/rcwarn/RCWarn.java
+++ src/main/java/de/raidcraft/rcwarn/RCWarn.java
@@ -13,6 +13,9 @@ import de.raidcraft.rcwarn.database.PointsTable;
 import de.raidcraft.rcwarn.database.ReasonsTable;
 import de.raidcraft.rcwarn.listener.PlayerListener;
 
+import java.net.URL;
+import java.net.URLConnection;
+
 /**
  * @author Philip
  */
@@ -62,10 +65,22 @@ public class RCWarn extends BasePlugin {
         @Setting("ban-text") public String banText = "Du wurdest %e gebannt! Informiere Dich im Forum! (forum.raid-craft.de)";
         @Setting("warning-cooldown") public int warningCooldown = 180;
         @Setting("supporter-max-warn-points") public int supporterMaxWarnPoints = 2;
+        @Setting("postbot-url") public String postbotURL = "http://apps.raid-craft.de/woltlab_postbot/scripts/rcwarn/rcwarn.php";
 
         public LocalConfiguration(RCWarn plugin) {
 
             super(plugin, "config.yml");
+        }
+    }
+
+    public void postThreads() {
+        try {
+            URL myURL = new URL(config.postbotURL);
+            URLConnection myURLConnection = myURL.openConnection();
+            myURLConnection.connect();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
