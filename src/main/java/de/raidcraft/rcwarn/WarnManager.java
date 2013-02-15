89cbb01f048f94946d0f1f5782a89b23a8845611
Philip Urban <urban.philip@googlemail.com>
2013-02-15 18:24:02 +0100

    fixed praise/warn limit

--- src/main/java/de/raidcraft/rcwarn/WarnManager.java
+++ src/main/java/de/raidcraft/rcwarn/WarnManager.java
@@ -61,14 +61,14 @@ public class WarnManager {
         }
         int points = Database.getTable(PointsTable.class).getAllPoints(player.getName());
         player.sendMessage(ChatColor.YELLOW + "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-        if(warning.getReason().getPoints() > 0)
+        if(warning.getReason().getPoints() > -1)
             player.sendMessage(ChatColor.RED.toString() + ChatColor.ITALIC + "Du wurdest verwarnt!");
         else
             player.sendMessage(ChatColor.GREEN.toString() + ChatColor.ITALIC + "Du wurdest gelobt!");
         player.sendMessage(ChatColor.YELLOW + "Grund: " + ChatColor.RED + warning.getReason().getName() + " (" + detail + ")");
         player.sendMessage(ChatColor.YELLOW + "Punkte: " + ChatColor.RED + warning.getReason().getPoints() +
                 " (Nächster Ban: " + points + "/" + BanManager.INST.getNextBanLevel(points).getPoints() + ")");
-        if(warning.getReason().getPoints() > 0)
+        if(warning.getReason().getPoints() > -1)
             player.sendMessage(ChatColor.RED + "Gebe " + ChatColor.WHITE + "/rcconfirm" + ChatColor.RED + " ein um die Warnung zur Kenntnis zu nehmen!");
         else
             player.sendMessage(ChatColor.GREEN + "Gebe " + ChatColor.WHITE + "/rcconfirm" + ChatColor.RED + " ein um das Lob zur Kenntnis zu nehmen!");
